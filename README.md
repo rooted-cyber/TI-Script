5c48a166c9726af542c5be698d6ba62e0fe6a4bb
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-06-14 22:39:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # TI script
 
-<img src="https://github.com/rooted-cyber/image-upload/raw/master/image-my.png" style="width:300px;height:400px;">
+<img src="https://github.com/rooted-cyber/TI-Script/raw/master/Ti.png" style="width:300px;height:400px;">
+<img src="https://github.com/rooted-cyber/TI-Script/raw/master/Ti2.png" style="width:300px;height:400px;">
 <pre>
  $ apt update
  $ apt upgrade
  $ apt-get install git
+ $ apt-get install figlet
+ $ apt-get install curl
+ $ apt-get install python
+ $ cd $HOME
  $ git clone https://github.com/rooted-cyber/TI-Script
  $ cd TI-Script
  $ bash TI.sh
  </pre>
- 
- <pre>
- start command :- TI </pre>
--- TI.sh
+++ TI.sh
@@ -1,8 +1,56 @@
-ho () {
-	echo
-	echo -e "Press enter to home"|lolcat
-	read
-	TI
+folder () {
+	cd $PREFIX/bin
+	}
+	ch () {
+		folder
+		if [ -e random ];then
+		echo
+		else
+		printf "#!/data/data/com.termux/files/usr/bin/python3\nimport random\nimport os\ncolor=['\033[1;91m','\033[1;92m','\033[1;93m','\033[1;94m','\033[1;95m','\033[1;96m']\nprint(random.choice(color))\nexit()" >> $PREFIX/bin/random
+		chmod 777 $PREFIX/bin/random
+		fi
+		}
+		p-c() {
+			folder
+			if [ -e python ];then
+			echo
+			else
+			printf "\n\033[1;92m Installing python\n"
+			pkg install python
+			fi
+			folder
+			if [ -e wget ];then
+			echo
+			else
+			random
+			printf "\n\nInstalling wget\n"
+			pkg install wget
+			fi
+			folder
+			if [ -e figlet ];then
+			echo
+			else
+			random
+			printf "\n\n Installing figlet \n"
+			pkg install figlet
+			fi
+			folder
+			if [ -e curl ];then
+			echo
+			else
+			random
+			printf "\n\n Installing curl \n"
+			pkg install curl
+			fi
+			}
+banner() {
+	clear
+	ch
+	p-c
+	clear
+	random
+	figlet TI-Script
+	printf "\n\033[1;92m[\033[0m::\033[1;92m]	\033[1;93mCreated by : \033[1;97mhttps://github.com/rooted-cyber	\033[1;92m[\033[0m::\033[1;92m]\n"
 	}
 	fake-else () {
 		cd ~/TI-Script
@@ -24,73 +72,56 @@ ho () {
 			unzip b.zip
 			fi
 			}
-		
-		
-ip () {
-	printf "Your Ip :- "|lolcat
-	curl ifconfig.me
-	echo
-	echo -e "Press enter to home"|lolcat
-	read
-	TI
-	}
-	T-Storage () {
-		clear
-		cd $HOME
-		du -hs *|sort -rf|head -800|lolcat
-		echo
-		du -hs
-		ho
-		}
-		I-Storage () {
-			clear
-			cd /sdcard
-			du -hs *|sort -rf | head -10000 |lolcat
-			echo
-			du -hs
-			ho
-			}
-			T-Tool () {
-				clear
-				printf " Your Termux Tool Info \n"|lolcat
-				cd $HOME
-				ls -a --full-time|lolcat
-				echo
-				printf "without color press enter"
-				read
-				ls -a --full-time
-				ho
+			phone () {
+				banner
+				
+				kernal="$(uname -r)"
+				ver="$(getprop ro.build.version.release)"
+				sdk="$(getprop ro.build.version.sdk)"
+				arch="$(getprop ro.product.cpu.abilist)"
+				cpu="$(getprop ro.board.platform)"
+				phone2="$(getprop ro.product.brand)"
+				phone="$(getprop ro.product.name)"
+				ip="$(curl -s -N ifconfig.me)"
+				
+				
+				printf "\n\n\033[1;92m Your phone name :\033[0m $phone2\n"
+				printf "\033[1;92m Your phone name & modal :\033[0m $phone\n"
+				printf "\033[1;92m Android Version :\033[0m $ver\n"
+				printf "\033[1;92m CPU :\033[0m $cpu\n"
+				printf "\033[1;92m Architecture :\033[0m $arch\n"
+				printf "\033[1;92m SDK :\033[0m SDK$sdk\n"
+				printf "\033[1;92m Kernal :\033[0m $kernal\n"
+				
+				printf "\033[1;92m Your ip :\033[1;97m $ip\n"
 				}
-				phone () {
-					clear
-					screenfetch
+				c2() {
 					echo
-					neofetch
-					ho
+					echo -e -n "\033[1;92m Enter folder name :\033[0m "
+					read ab
+					cd /sdcard
+					#du -hs *|sort -rf |head -1000
+					if [ ! -z $a ];then
+					cd ~
+					du -hs *|sort -rf |head -1000|grep -e "$ab"
+					fi
 					}
-					filter () {
-						toilet -F list
-						ho
-						}
-						export () {
-							toilet -E list
-							ho
-							}
-							full-s () {
+				custom() {
+					echo
+					echo -e -n "\033[1;92m Enter folder name :\033[0m "
+					read a
+					cd /sdcard
+					#du -hs *|sort -rf |head -1000
+					if [ ! -z $a ];then
+					cd /sdcard
+					du -hs *|sort -rf |head -1000|grep -e "$a"
+					fi
+					}
+			full-s () {
 								cd ~/TI-Script
 								el
 								bash .b.sh
-								ho
 								}
-								arch () {
-									printf "Your architecture :- "|lolcat
-									dpkg --print-architecture
-									ho
-									}
-									kernal () {
-										print " Your Version :- \n\n"
-										uname -a
-										}
 										full-fake () {
 											cd ~/TI-Script
 											el
@@ -100,68 +131,25 @@ ip () {
 											cp -f .c.sh ~/TI-Script/.fake-storage
 											cd .fake-storage
 											bash .c.sh
-											ho
 											}
-cd $PREFIX/bin
-if [ -e toilet ];then
-echo
-else
-pkg install toilet
-clear
-fi
-if [ -e TI ];then
-echo
-else
-printf "\033[92m [+] Requirements installing...."
-apt update
-apt upgrade
-apt install screenfetch
-apt install neofetch
-apt install curl
-apt install python
-pip install lolcat
-echo "#!/data/data/com.termux/files/usr/bin/sh" >> $PREFIX/bin/TI
-echo "cd ~/TI-Script" >> $PREFIX/bin/TI
-echo "bash TI.sh" >> $PREFIX/bin/TI
-chmod 777 $PREFIX/bin/TI
-clear
-echo -e "Now you can use this command :- TI "
-printf "Press enter to start"
-read
-fi
-cd ~/TI-Script
-hara="\033[92m"
-lal="\033[91m"
-sada="\033[97m"
-pila="\033[93m"
-toilet -F metal TI-script
-echo
-printf "	$hara [ $sada 1 $hara ] $pila Your ip\n"
-printf "	$hara [ $sada 2 $hara ] $pila Termux-storage\n"
-printf "	$hara [ $sada 3 $hara ] $pila internal storage info\n"
-printf "	$hara [ $sada 4 $hara ] $pila Kernal version\n"
-printf "	$hara [ $sada 5 $hara ] $pila Termux-Tool Info\n"
-printf "	$hara [ $sada 6 $hara ] $pila phone info\n"
-printf "	$hara [ $sada 7 $hara ] $pila Your architecture\n"
-printf "	$hara [ $sada 8 $hara ] $pila Toilet filter list\n"
-printf "	$hara [ $sada 9 $hara ] $pila Toilet export list\n"
-printf "	$hara [ $sada 10 $hara ] $pila Storage full\n"
-printf "	$hara [ $sada 11 $hara ] $pila Termux-Fake-Storage \n"
-printf "	$hara [ $sada 12 $hara ] $pila Exit\n\n\n"
-echo -e -n "\033[93m TI@(\033[91m Script\033[93m ) >>  "
-read a
-case $a in
-1)ip ;;
-2)T-Storage ;;
-3)I-Storage ;;
-4)kernal ;;
-5)T-Tool ;;
-6)phone ;;
-7)arch ;;
-8)filter ;;
-9)export ;;
-10)full-s ;;
-11)full-fake ;;
-12)exit ;;
-*)TI ;;
-esac
+menu () {
+	banner
+	printf "\n\033[1;91m[\033[0m1\033[1;91m]\033[1;96m Your Phone information\n"
+	printf "\033[1;91m[\033[0m2\033[1;91m]\033[1;96m Check custom directory size\033[1;91m (\033[0m/sdcard\033[1;91m)\n"
+	printf "\033[1;91m[\033[0m3\033[1;91m]\033[1;96m Check custom directory size\033[1;91m (\033[0m$HOME\033[1;91m)\n"
+	printf "\033[1;91m[\033[0m4\033[1;91m]\033[1;96m Storage Full\n"
+	printf "\033[1;91m[\033[0m5\033[1;91m]\033[1;96m Termux Fake Storage\n"
+	printf "\033[1;91m[\033[0m6\033[1;91m]\033[1;96m Exit\n\n\n"
+	echo -e -n "\033[1;93mTI\033[0m@\033[1;92mScript\033[1;97m -->> "
+	read a
+	case $a in
+	1)phone ;;
+	2)custom ;;
+	3)c2 ;;
+	4)full-s ;;
+	5)full-fake ;;
+	6)exit ;;
+	*)menu ;;
+	esac
+	}
+	menu
